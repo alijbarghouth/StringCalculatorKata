02bd86f39107e8900fdebce234429cb889f5bb50
alijbarghouth <alijbarghouth@gmail.com>
2023-04-02 02:08:46 +0300

    Add Class String Calc And Method Add And Tow Private Method
    	modified:   String_Calculator_Kata/Program.cs
    	new file:   String_Calculator_Kata/StringCalculator/IStringCalculator.cs
    	new file:   String_Calculator_Kata/StringCalculator/StringCalculator.cs
    	modified:   Tests/StringCalculatorKataTest/StringCalculatorKataTest.csproj
    	new file:   Tests/StringCalculatorKataTest/StringCalculatorTest.cs
    	deleted:    Tests/StringCalculatorKataTest/UnitTest1.cs
    
    	modified:   String_Calculator_Kata/Program.cs
    	new file:   String_Calculator_Kata/StringCalculator/IStringCalculator.cs
    	new file:   String_Calculator_Kata/StringCalculator/StringCalculator.cs
    	modified:   Tests/StringCalculatorKataTest/StringCalculatorKataTest.csproj
    	new file:   Tests/StringCalculatorKataTest/StringCalculatorTest.cs
    	deleted:    Tests/StringCalculatorKataTest/UnitTest1.cs
    
    	modified:   String_Calculator_Kata/Program.cs
    	new file:   String_Calculator_Kata/StringCalculator/IStringCalculator.cs
    	new file:   String_Calculator_Kata/StringCalculator/StringCalculator.cs
    	modified:   Tests/StringCalculatorKataTest/StringCalculatorKataTest.csproj
    	new file:   Tests/StringCalculatorKataTest/StringCalculatorTest.cs
    	deleted:    Tests/StringCalculatorKataTest/UnitTest1.cs
    
    	modified:   String_Calculator_Kata/Program.cs
    	new file:   String_Calculator_Kata/StringCalculator/IStringCalculator.cs
    	new file:   String_Calculator_Kata/StringCalculator/StringCalculator.cs
    	modified:   Tests/StringCalculatorKataTest/StringCalculatorKataTest.csproj
    	new file:   Tests/StringCalculatorKataTest/StringCalculatorTest.cs
    	deleted:    Tests/StringCalculatorKataTest/UnitTest1.cs
    
    	modified:   String_Calculator_Kata/Program.cs
    	new file:   String_Calculator_Kata/StringCalculator/IStringCalculator.cs
    	new file:   String_Calculator_Kata/StringCalculator/StringCalculator.cs
    	modified:   Tests/StringCalculatorKataTest/StringCalculatorKataTest.csproj
    	new file:   Tests/StringCalculatorKataTest/StringCalculatorTest.cs
    	deleted:    Tests/StringCalculatorKataTest/UnitTest1.cs
    
    	modified:   String_Calculator_Kata/Program.cs
    	new file:   String_Calculator_Kata/StringCalculator/IStringCalculator.cs
    	new file:   String_Calculator_Kata/StringCalculator/StringCalculator.cs
    	modified:   Tests/StringCalculatorKataTest/StringCalculatorKataTest.csproj
    	new file:   Tests/StringCalculatorKataTest/StringCalculatorTest.cs
    	deleted:    Tests/StringCalculatorKataTest/UnitTest1.cs

---
+++ String_Calculator_Kata/StringCalculator/StringCalculator.cs
@@ -0,0 +1,66 @@
+﻿namespace StringCalculatorKata.StringCalculator
+{
+    public class StringCalculator : IStringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrWhiteSpace(numbers))
+                return 0;
+            var customDelimeter = CustomDelimeter(numbers);
+            var sum = Sum(numbers, customDelimeter);
+
+            return sum;
+        }
+
+        private int Sum(string numbers, char customDelimeter)
+        {
+            var listArrayOfNumbers = ConvartStringToArrayOfNumber(numbers, customDelimeter);
+
+            return listArrayOfNumbers.Sum(x => x);
+        }
+        private char CustomDelimeter(string numbers)
+        {
+            return numbers.StartsWith("//") ? numbers[2] : ',';
+        }
+        private List<int> ConvartStringToArrayOfNumber(string numbers, char customDelimeter)
+        {
+            var listNumbers = ConvartStringToArrayWithAtMostTowNumberOfTheArray(numbers, customDelimeter);
+            var isNegativNumber = HasNegativeNumber(listNumbers);
+            ThrowAnExceptionIfFindNegativNumber(isNegativNumber, listNumbers);
+
+            return listNumbers;
+        }
+
+        private void ThrowAnExceptionIfFindNegativNumber(bool isNegativNumber, List<int> numbersWithoutEmptySpace)
+        {
+            if (isNegativNumber)
+                throw new ArgumentException($"negatives not allowed: " +
+                    $"{ReturnStirngContainTheNumberNegative(numbersWithoutEmptySpace)}");
+        }
+        private List<int> ConvartStringToArrayWithAtMostTowNumberOfTheArray(string numbers, char customDelimeter)
+        {
+            var delimeter = new char[] { '\n', '/' }.ToList();
+            delimeter.Add(customDelimeter);
+            var number = numbers.Split(delimeter.ToArray()).ToList();
+
+            return number
+                .Where(x => !string.IsNullOrEmpty(x))
+                .Select(x => int.Parse(x))
+                .Where(x => x <= 1000)
+                .ToList();
+        }
+        private string ReturnStirngContainTheNumberNegative(List<int> numbersWithoutEmptySpace)
+        {
+            string nums = "";
+            numbersWithoutEmptySpace.Where(x => x < 0)
+                .ToList().ForEach(x => nums += x + ",");
+
+            return nums;
+        }
+        private bool HasNegativeNumber(List<int> number)
+        {
+            return number
+                .Any(x => x < 0);
+        }
+    }
+}
---
+++ Tests/StringCalculatorKataTest/StringCalculatorTest.cs
@@ -0,0 +1,68 @@
+using StringCalculatorKata.StringCalculator;
+
+namespace StringCalculatorKataTest
+{
+    public class StringCalculatorTest
+    {
+        private readonly IStringCalculator _stringCalculator;
+
+        public StringCalculatorTest()
+        {
+            _stringCalculator = new StringCalculator();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData(null)]
+        public void Add_ShouldReturnZero(string numbers)
+        {
+            var actual = _stringCalculator.Add(numbers);
+
+            Assert.Equal(0, actual);
+        }
+
+        [Theory]
+        [InlineData("1",1)]
+        [InlineData("1,2",3)]
+        public void Add_ShouldReturnTheSum(string numbers,int expected)
+        {
+            Assert.Equal(expected, _stringCalculator.Add(numbers));
+        }
+        [Theory]
+        [InlineData("1,2,3,4",10)]
+        [InlineData("1,2,3,4,5",15)]
+        public void Add_ShouldReturnTheSumAllowUnknownAmountOfNumbers(string numbers, int expected)
+        {
+            Assert.Equal(expected, _stringCalculator.Add(numbers));
+        }
+        [Theory]
+        [InlineData("1\n2,3", 6)]
+        [InlineData("1\n2,3\n5",11)]
+        public void Add_ShouldReturnTheSumHandleNewLinesBetweenNumbers(string numbers,int expected)
+        {
+            Assert.Equal(1, _stringCalculator.Add("1"));
+            Assert.Equal(3, _stringCalculator.Add("1,2"));           
+        }
+
+        [Theory]
+        [InlineData("//;\n1;2", 3)]
+        [InlineData("//;;;;;;1\n\n7\n1;2", 11)]
+        public void Add_SupportDifferentDelimiters(string numbers,int expected)
+        {
+            Assert.Equal(expected, _stringCalculator.Add(numbers));
+        }
+
+        [Fact]
+        public void Add_NegativeNumbersMustReturnException()
+        {
+            Assert.Throws<ArgumentException>(() => _stringCalculator.Add("1,4,-1"));
+        }
+
+        [Fact]
+        public void Add_IgnoreBigNumbers()
+        {
+            Assert.Equal(3, _stringCalculator.Add("1,1001,2000,2"));
+        }
+    }
+}
