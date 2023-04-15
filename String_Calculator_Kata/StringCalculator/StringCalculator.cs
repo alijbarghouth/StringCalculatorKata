a759056391530a1d4dff27655a7c6a6b61af6d67
alijbarghouth <alijbarghouth@gmail.com>
2023-04-16 01:10:17 +0300

    sol conflict

--- String_Calculator_Kata/StringCalculator/StringCalculator.cs
+++ String_Calculator_Kata/StringCalculator/StringCalculator.cs
@@ -2,26 +2,21 @@
 {
     public class StringCalculator : IStringCalculator
     {
-        public int Add(string numbers)
+        public int Add(string inputs)
         {
-            if (string.IsNullOrWhiteSpace(numbers))
+            if (string.IsNullOrWhiteSpace(inputs))
                 return 0;
-            var sum = Sum(numbers);
+            var sum = Convart(inputs);
 
-            return sum;
+            return sum.Sum(x => x);
         }
 
-        private int Sum(string numbers)
-        {
-            var listArrayOfNumbers = ConvartStringToArrayOfNumber(numbers);
 
-            return listArrayOfNumbers.Sum(x => x);
-        }
-        private List<int> ConvartStringToArrayOfNumber(string numbers)
+        private List<int> Convart(string inputs)
         {
-            var listOfNumbers = numbers.Split(',');
+            var numbers = inputs.Split(',').ToList();
 
-             return listOfNumbers
+             return numbers
                 .Select(x => int.Parse(x))
                 .ToList(); 
         }
--- Tests/StringCalculatorKataTest/StringCalculatorTest.cs
+++ Tests/StringCalculatorKataTest/StringCalculatorTest.cs
@@ -12,29 +12,31 @@ namespace StringCalculatorKataTest
         }
 
         [Theory]
-        [InlineData("")]
-        [InlineData("   ")]
-        [InlineData(null)]
-        public void Add_ShouldReturnZero(string numbers)
+        [InlineData("", 0)]
+        [InlineData("   ", 0)]
+        [InlineData(null, 0)]
+        [InlineData("1", 1)]
+        [InlineData("1,2", 3)]
+        [InlineData("1,2,4,5,6", 18)]
+        [InlineData("1,2,4,5,6,7", 25)]
+        public void Add_Success(string numbers, int expected)
         {
             var actual = _stringCalculator.Add(numbers);
 
-            Assert.Equal(0, actual);
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData("1",1)]
-        [InlineData("1,2",3)]
-        public void Add_ShouldReturnTheSum(string numbers,int expected)
+        [InlineData("", 1)]
+        [InlineData("   ", 2)]
+        [InlineData(null, 4)]
+        [InlineData("1", 11)]
+        [InlineData("1,2", 23)]
+        public void Add_Failure(string numbers, int expected)
         {
-            Assert.Equal(expected, _stringCalculator.Add(numbers));
-        }
-        [Theory]
-        [InlineData("1,2,3,4", 10)]
-        [InlineData("1,2,3,4,5", 15)]
-        public void Add_ShouldReturnTheSumAllowUnknownAmountOfNumbers(string numbers, int expected)
-        {
-            Assert.Equal(expected, _stringCalculator.Add(numbers));
+            var actual = _stringCalculator.Add(numbers);
+
+            Assert.NotEqual(expected, actual);
         }
     }
 }
